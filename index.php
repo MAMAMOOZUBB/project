7004864741b29a9f159ed4e6492a2de123de220d
MAMAMOOZUBB <jakkrit308@gmail.com>
2025-01-08 00:46:41 +0700

    +H

--- index.php
+++ index.php
@@ -8,14 +8,21 @@ $result = $conn->query($sql);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Table from Database</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <h1>User Table</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
     <table>
         <thead>
             <tr>
@@ -26,7 +33,7 @@ $result = $conn->query($sql);
         </thead>
         <tbody>
             <?php if ($result->num_rows > 0): ?>
-                <?php while($row = $result->fetch_assoc()): ?>
+                <?php while ($row = $result->fetch_assoc()): ?>
                     <tr>
                         <td><?= $row['id']; ?></td>
                         <td><?= $row['name']; ?></td>
@@ -41,4 +48,5 @@ $result = $conn->query($sql);
         </tbody>
     </table>
 </body>
+
 </html>
